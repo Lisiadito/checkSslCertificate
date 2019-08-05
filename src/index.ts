40883d21762fd59c8587b8a25611bdaecce1d278
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2019-08-05 21:12:02 +0200

    add function and tests

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import * as https from 'https'
+import { TLSSocket } from 'tls'
+
+export interface UrlObject {
+    method?: 'HEAD' | 'GET'
+    path?: string
+    port?: number
+    hostname: string
+}
+
+export interface SslCheckResponse {
+    error?: string
+    obj: UrlObject
+    valid: boolean
+    validFrom?: string
+    validUntil?: string
+}
+
+export default function checkSslCertificate(obj: UrlObject): Promise<SslCheckResponse> {
+    return new Promise<SslCheckResponse>(resolve => {
+        if (!obj.hostname || obj.hostname === '') {
+            resolve({error:'Host missing', obj, valid: false})
+        }
+
+        const options = {
+            hostname: obj.hostname,
+            path: obj.path || '',
+            port: obj.port || 443,
+            method: obj.method || 'HEAD'
+        }
+
+        const req = https.request(options, res => {
+            if (res.statusCode !== 200) {
+               resolve({error:(res.statusCode || '').toString(), obj, valid: false})
+            }
+
+            const connection = res.connection as TLSSocket
+            const validFrom = connection.getPeerCertificate().valid_from
+            const validUntil = connection.getPeerCertificate().valid_to
+
+            resolve({ valid: true, validFrom, validUntil, obj })
+        })
+
+        req.on('error', error => {
+            resolve({ valid: false, obj, error: error.message })
+        })
+
+        req.end()
+    })
+}
---
+++ test/test.js
@@ -0,0 +1,43 @@
+const expect = require('chai').expect
+const checkSslCertificate = require("../dist/index").default
+
+describe('checkSslCertificate', function() {
+  it('should resolve when passing only a hostname', async () => {
+    const result = await checkSslCertificate({hostname: 'github.com'})
+    expect(result).to.be.a('Object')
+    expect(result).to.have.property('obj')
+    expect(result).to.have.property('valid', true)
+    expect(result).to.have.property('validFrom')
+    expect(result).to.have.property('validUntil')
+    expect(result.obj).to.have.property('hostname')
+    expect(result.obj.hostname).to.equal('github.com')
+  })
+
+  it('should resolve when passing a different method', async () => {
+    const result = await checkSslCertificate({hostname: 'github.com', method: 'GET'})
+    expect(result).to.be.a('Object')
+    expect(result).to.have.property('obj')
+    expect(result).to.have.property('valid', true)
+    expect(result).to.have.property('validFrom')
+    expect(result).to.have.property('validUntil')
+    expect(result.obj).to.have.property('hostname')
+    expect(result.obj.hostname).to.equal('github.com')
+  })
+
+  it('should resolve with an error when empty hostname is passed', async () => {
+    const result = await checkSslCertificate({hostname: ''})
+    expect(result).to.have.property('error')
+    expect(result.error).to.equal('Host missing')
+  })
+
+  it('should resolve with an error when path is contained in the hostname', async () => {
+    const result = await checkSslCertificate({hostname: 'github.com/foo'})
+    expect(result).to.have.property('error')
+  })
+
+  it('should resolve with an error when response code not 200', async () => {
+    const result = await checkSslCertificate({hostname: 'httpstat.us', path: '/400'})
+    expect(result).to.have.property('error')
+    expect(result.error).to.have.equal('400')
+  })
+})
